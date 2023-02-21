e581466bb80c1864c65c440bcb18cf917ab9b481
rizkypsr <rizkypsrr@gmail.com>
2023-02-21 17:06:49 +0700

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#069DD9",
+        secondary: "#5BC7EA",
+        "light-red": "#FFDADA",
+        "dark-red": "#EB5757",
+        "lihgt-blue": "#E2F8FF",
+        "dark-gray": "#333333",
+        "light-gray": "#828282",
+        "cotton-ball": "#F3F6FF",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
