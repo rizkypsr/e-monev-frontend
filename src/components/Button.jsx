1c4eedb5ec668dbea795424ca270bc180df41720
rizkypsr <rizkypsrr@gmail.com>
2023-03-08 10:04:46 +0700

    add button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Button({
+  children,
+  className,
+  type = "button",
+  icon,
+  onClick,
+  background,
+  textColor,
+}) {
+  return type === "modal" ? (
+    <div
+      onClick={onClick}
+      className={`font-medium rounded-lg text-sm px-3 min-w-fit py-2.5 ${
+        icon ? "flex items-center justify-center space-x-2" : ""
+      } ${background ? background : "bg-transparent"} ${className} ${
+        textColor ?? "text-dark-gray"
+      }`}>
+      {icon}
+      <span>{children}</span>
+    </div>
+  ) : (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`font-medium rounded-lg text-sm px-3 min-w-fit py-2.5 ${
+        icon ? "flex items-center justify-center space-x-2" : ""
+      } ${background ? background : "bg-transparent"} ${className} ${
+        textColor ?? "text-dark-gray"
+      }`}>
+      {icon}
+      <span>{children}</span>
+    </button>
+  );
+}
+
+export default Button;
