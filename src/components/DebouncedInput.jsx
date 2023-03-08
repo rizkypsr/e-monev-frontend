2f1462d4383669ad411ba785961aa28ba739b534
rizkypsr <rizkypsrr@gmail.com>
2023-03-08 10:05:00 +0700

    add debounced input compnent

---
+++ src/components/DebouncedInput.jsx
@@ -0,0 +1,37 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import React, { useEffect, useState } from "react";
+
+function DebouncedInput({ initialValue, onChange, debounce = 500, ...props }) {
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    setValue(initialValue);
+    console.log(initialValue);
+  }, [initialValue]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
+
+    return () => clearTimeout(timeout);
+  }, [value]);
+
+  return (
+    <>
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <MagnifyingGlassIcon className="w-4 h-4" />
+      </div>
+      <input
+        {...props}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type="search"
+        className="bg-gray-50 text-light-gray border-none text-sm rounded-lg focus:ring-0 block w-full pl-10 p-2.5 shadow"
+        placeholder="Pencarian"
+      />
+    </>
+  );
+}
+
+export default DebouncedInput;
