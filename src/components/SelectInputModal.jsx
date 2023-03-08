a3cb78ae2ddcad85f17cc5aad3f55a3a37f85d54
rizkypsr <rizkypsrr@gmail.com>
2023-03-08 10:06:43 +0700

    add select input modal

---
+++ src/components/SelectInputModal.jsx
@@ -0,0 +1,14 @@
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import React from "react";
+
+function SelectInputModal({ className, selectedValue, label }) {
+  return (
+    <div
+      className={`flex text-sm items-center justify-between border border-dark-gray rounded-lg p-2.5 leading-4 text-[#828282] ${className}`}>
+      <span>{selectedValue || label}</span>
+      <ChevronDownIcon className="w-3 h-3" />
+    </div>
+  );
+}
+
+export default SelectInputModal;
