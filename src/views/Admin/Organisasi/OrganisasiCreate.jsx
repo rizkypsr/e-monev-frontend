3329c4ced3da39985bb75b1d13c1104a734bb247
rizkypsr <rizkypsrr@gmail.com>
2023-03-10 08:34:59 +0700

    post api

--- src/views/Admin/Organisasi/OrganisasiCreate.jsx
+++ src/views/Admin/Organisasi/OrganisasiCreate.jsx
@@ -1,11 +1,60 @@
 import { ArrowLeftIcon, CheckCircleIcon } from "@heroicons/react/24/solid";
 import { Label } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import TextInput from "../../../components/TextInput";
 import Button from "../../../components/Button";
+import { baseUrl } from "../../../utils/constants";
+import { toast } from "react-toastify";
+import { useToastContext } from "../../../context/ToastContext";
+import { useAuthHeader } from "react-auth-kit";
 
 function OrganisasiCreate() {
+  const [kode, setKode] = useState("");
+  const [organisasi, setOrganisasi] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const { showToastMessage } = useToastContext();
+  const navigate = useNavigate();
+  const authHeader = useAuthHeader();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(`${baseUrl}/org/create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: authHeader(),
+        },
+        body: JSON.stringify({ code: kode, title: organisasi }),
+      });
+      const result = await response.json();
+      setLoading(false);
+      if (response.ok) {
+        showToastMessage("Organisasi berhasil ditambahkan!");
+        navigate("../");
+      } else {
+        setError(new Error(result.message));
+        toast.error("Terjadi kesalahan pada server", {
+          position: toast.POSITION.BOTTOM_CENTER,
+          autoClose: 3000,
+        });
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+      toast.error("Terjadi kesalahan pada server", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 3000,
+      });
+    }
+  }
+
   return (
     <>
       <div className="flex justify-between">
@@ -21,8 +70,10 @@ function OrganisasiCreate() {
           </h1>
         </Link>
 
-        <form className="mt-4">
-          <div className="mb-6">
+        <form
+          className="mt-4"
+          onSubmit={handleSubmit}>
+          {/* <div className="mb-6">
             <Label>Jenis</Label>
             <TextInput
               className="mt-2 lg:w-2/3 xl:w-1/3"
@@ -37,14 +88,15 @@ function OrganisasiCreate() {
               placeholder="Masukan Induk"
               required={true}
             />
-          </div>
+          </div> */}
           <div className="mb-6">
             <Label>Kode</Label>
             <TextInput
               className="mt-2 lg:w-2/3 xl:w-1/3"
-              value={29833}
-              required={true}
-              disabled={true}
+              placeholder="Masukan Kode"
+              value={kode}
+              onChange={(e) => setKode(e.target.value)}
+              required
             />
           </div>
           <div className="mb-6">
@@ -52,11 +104,14 @@ function OrganisasiCreate() {
             <TextInput
               className="mt-2 lg:w-2/3 xl:w-1/3"
               placeholder="Masukan Organisasi"
-              required={true}
+              value={organisasi}
+              onChange={(e) => setOrganisasi(e.target.value)}
+              required
             />
           </div>
           <div className="flex space-x-3">
             <Button
+              type="submit"
               className="w-full md:w-28"
               background="bg-primary"
               textColor="text-white"
