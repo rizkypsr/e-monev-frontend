cef259608b7af7ea0b2a0c935a68b9bfa0c9470e
rizkypsr <rizkypsrr@gmail.com>
2023-03-10 08:34:52 +0700

    fetching API

--- src/views/Admin/Organisasi/OrganisasiTable.jsx
+++ src/views/Admin/Organisasi/OrganisasiTable.jsx
@@ -5,7 +5,6 @@ import {
   PlusIcon,
   TrashIcon,
 } from "@heroicons/react/24/solid";
-import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import {
   createColumnHelper,
   getCoreRowModel,
@@ -13,24 +12,73 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthHeader } from "react-auth-kit";
 import { Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import Button from "../../../components/Button";
 import Dropdown from "../../../components/Dropdown";
 import Table from "../../../components/Table";
-import { makeData } from "./makeData";
+import { useToastContext } from "../../../context/ToastContext";
+import { baseUrl } from "../../../utils/constants";
 
 function OrganisasiTable() {
-  const columnHelper = createColumnHelper();
-  const [data, setData] = useState(() => makeData(1000));
+  const [data, setData] = useState([]);
   const [sorting, setSorting] = useState([]);
+  const [pageSize, setPageSize] = useState(10);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const authHeader = useAuthHeader();
+  const { showToast, toastMessage, hideToastMessage } = useToastContext();
+
+  useEffect(() => {
+    if (showToast) {
+      toast.success(toastMessage, {
+        onClose: hideToastMessage,
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 3000,
+      });
+    }
+  }, [showToast, toastMessage, hideToastMessage]);
+
+  useEffect(() => {
+    fetchData(0, pageSize);
+  }, [pageSize]);
+
+  async function fetchData(offset, limit) {
+    try {
+      const response = await fetch(
+        `${baseUrl}/org/list?offset=${offset}&limit=${limit}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: authHeader(),
+          },
+        }
+      );
+      const jsonData = await response.json();
+      setData(jsonData.data.result);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+  }
+
+  const columnHelper = createColumnHelper();
   const columns = [
     columnHelper.accessor("id", {
       cell: (info) => info.getValue(),
       footer: (info) => info.column.id,
     }),
-    columnHelper.accessor((row) => row.organisasi, {
-      id: "organisasi",
+    columnHelper.accessor((row) => row.code, {
+      id: "code",
+      cell: (info) => <i>{info.getValue()}</i>,
+      header: () => <span>Kode</span>,
+    }),
+    columnHelper.accessor((row) => row.title, {
+      id: "title",
       cell: (info) => <i>{info.getValue()}</i>,
       header: () => <span>Organisasi</span>,
     }),
@@ -49,20 +97,20 @@ function OrganisasiTable() {
                 Edit
               </Button>
             </Link>
-            <Button
-              className="text-sm font-normal"
-              textColor="text-blue-500"
-              icon={<EyeIcon className="w-4 h-4" />}>
-              Lihat
-            </Button>
-            <AlertDialog.Trigger asChild>
+            <Link to={`detail/${rowId}`}>
               <Button
                 className="text-sm font-normal"
-                textColor="text-red-500"
-                icon={<TrashIcon className="w-4 h-4" />}>
-                Hapus
+                textColor="text-blue-500"
+                icon={<EyeIcon className="w-4 h-4" />}>
+                Lihat
               </Button>
-            </AlertDialog.Trigger>
+            </Link>
+            <Button
+              className="text-sm font-normal"
+              textColor="text-red-500"
+              icon={<TrashIcon className="w-4 h-4" />}>
+              Hapus
+            </Button>
           </div>
         );
       },
@@ -95,92 +143,91 @@ function OrganisasiTable() {
 
   return (
     <>
-      <AlertDialog.Root>
-        <div className="flex justify-between">
-          <h1 className="text-2xl font-semibold">Organisasi</h1>
-          <Link to="create">
-            <Button
-              background="bg-primary"
-              textColor={"text-white"}
-              icon={<PlusIcon className="w-4 h-4" />}>
-              Tambah Organisasi
-            </Button>
-          </Link>
-        </div>
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-semibold">Organisasi</h1>
+        <Link to="create">
+          <Button
+            background="bg-primary"
+            textColor={"text-white"}
+            icon={<PlusIcon className="w-4 h-4" />}>
+            Tambah Organisasi
+          </Button>
+        </Link>
+      </div>
 
-        <div className="flex justify-between mt-6">
-          <div className="flex space-x-3">
-            {/* Sorting Dropdown */}
-            <div>
-              <Dropdown
-                onSelect={onSorting}
-                defaultValue="A - Z"
-                label="Urutkan:">
-                <Dropdown.Items>
-                  <li
-                    value="asc"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    A - Z
-                  </li>
-                  <li
-                    value="desc"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    Z - A
-                  </li>
-                </Dropdown.Items>
-              </Dropdown>
-            </div>
-
-            {/* Page Size Dropdown */}
-            <div>
-              <Dropdown
-                onSelect={onPageSizeChanged}
-                defaultValue="10"
-                label="Tampilkan:"
-                endLabel="Entri">
-                <Dropdown.Items>
-                  <li
-                    value="10"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    10
-                  </li>
-                  <li
-                    value="50"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    50
-                  </li>
-                  <li
-                    value="100"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    100
-                  </li>
-                </Dropdown.Items>
-              </Dropdown>
-            </div>
+      <div className="flex justify-between mt-6">
+        <div className="flex space-x-3">
+          {/* Sorting Dropdown */}
+          <div>
+            <Dropdown
+              onSelect={onSorting}
+              defaultValue="A - Z"
+              label="Urutkan:">
+              <Dropdown.Items>
+                <li
+                  value="asc"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  A - Z
+                </li>
+                <li
+                  value="desc"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  Z - A
+                </li>
+              </Dropdown.Items>
+            </Dropdown>
           </div>
 
-          <div className="relative w-1/3">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <MagnifyingGlassIcon className="w-4 h-4" />
-            </div>
-            <input
-              type="search"
-              id="search"
-              className="bg-gray-50 text-light-gray border-none text-sm rounded-lg focus:ring-0 block w-full pl-10 p-2.5 shadow"
-              placeholder="Pencarian"
-            />
+          {/* Page Size Dropdown */}
+          <div>
+            <Dropdown
+              onSelect={onPageSizeChanged}
+              defaultValue="10"
+              label="Tampilkan:"
+              endLabel="Entri">
+              <Dropdown.Items>
+                <li
+                  value="10"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  10
+                </li>
+                <li
+                  value="50"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  50
+                </li>
+                <li
+                  value="100"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  100
+                </li>
+              </Dropdown.Items>
+            </Dropdown>
           </div>
         </div>
 
-        <div className="w-full h-full mt-6 bg-white rounded-lg">
-          <Table
-            className="mt-6"
-            table={table}
-            columns={columns}
-            data={data}
+        <div className="relative w-1/3">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <MagnifyingGlassIcon className="w-4 h-4" />
+          </div>
+          <input
+            type="search"
+            id="search"
+            className="bg-gray-50 text-light-gray border-none text-sm rounded-lg focus:ring-0 block w-full pl-10 p-2.5 shadow"
+            placeholder="Pencarian"
           />
         </div>
-      </AlertDialog.Root>
+      </div>
+
+      <div className="w-full h-full mt-6 bg-white rounded-lg">
+        <Table
+          className="mt-6"
+          table={table}
+          columns={columns}
+          data={data}
+        />
+      </div>
+      <ToastContainer />
     </>
   );
 }
