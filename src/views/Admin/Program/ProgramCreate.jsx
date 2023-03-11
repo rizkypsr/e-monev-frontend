2ab2f4e25180a928cffbd22721faad18aa2d5e11
rizkypsr <rizkypsrr@gmail.com>
2023-03-11 08:34:56 +0700

    add program table and create pages

--- src/views/Admin/Program/ProgramCreate.jsx
+++ src/views/Admin/Program/ProgramCreate.jsx
@@ -1,11 +1,54 @@
 import { ArrowLeftIcon, CheckCircleIcon } from "@heroicons/react/24/solid";
 import { Label } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import TextInput from "../../../components/TextInput";
 import Button from "../../../components/Button";
+import { useAuthHeader } from "react-auth-kit";
+import { useToastContext } from "../../../context/ToastContext";
+import { baseUrl } from "../../../utils/constants";
+import { makeRequest } from "../../../utils/makeRequest";
+import { toast } from "react-toastify";
 
 function ProgramCreate() {
+  const [kode, setKode] = useState("");
+  const [program, setProgram] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const authHeader = useAuthHeader();
+  const navigate = useNavigate();
+  const { showToastMessage } = useToastContext();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    setLoading(true);
+    setError(null);
+
+    const url = `${baseUrl}/program/create`;
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: authHeader(),
+    };
+    const body = { code: kode, title: program };
+
+    try {
+      await makeRequest(url, headers, "POST", body);
+
+      setLoading(false);
+      showToastMessage("Urusan berhasil ditambahkan!");
+      navigate("../");
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+
+      toast.error("Terjadi kesalahan pada server", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 3000,
+      });
+    }
+  }
+
   return (
     <>
       <div className="flex justify-between">
@@ -21,14 +64,17 @@ function ProgramCreate() {
           </h1>
         </Link>
 
-        <form className="mt-4">
+        <form
+          className="mt-4"
+          onSubmit={handleSubmit}>
           <div className="mb-6">
             <Label>Kode</Label>
             <TextInput
               className="mt-2 lg:w-2/3 xl:w-1/3"
-              value={29833}
-              required={true}
-              disabled={true}
+              value={kode}
+              placeholder="Masukan Kode Program"
+              onChange={(e) => setKode(e.target.value)}
+              required
             />
           </div>
           <div className="mb-6">
@@ -36,11 +82,14 @@ function ProgramCreate() {
             <TextInput
               className="mt-2 lg:w-2/3 xl:w-1/3"
               placeholder="Masukan Program"
+              value={program}
+              onChange={(e) => setProgram(e.target.value)}
               required={true}
             />
           </div>
           <div className="flex space-x-3">
             <Button
+              type="submit"
               className="w-full md:w-28"
               background="bg-primary"
               textColor="text-white"
--- src/views/Admin/Program/ProgramTable.jsx
+++ src/views/Admin/Program/ProgramTable.jsx
@@ -5,7 +5,6 @@ import {
   PlusIcon,
   TrashIcon,
 } from "@heroicons/react/24/solid";
-import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import {
   createColumnHelper,
   getCoreRowModel,
@@ -13,23 +12,65 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthHeader } from "react-auth-kit";
 import { Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import Button from "../../../components/Button";
 import Dropdown from "../../../components/Dropdown";
 import Table from "../../../components/Table";
-import { makeData } from "./makeData";
+import { useToastContext } from "../../../context/ToastContext";
+import { baseUrl } from "../../../utils/constants";
+import { makeRequest } from "../../../utils/makeRequest";
 
 function ProgramTable() {
-  const columnHelper = createColumnHelper();
-  const [data, setData] = useState(() => makeData(1000));
+  const [data, setData] = useState([]);
   const [sorting, setSorting] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [pageSize, setPageSize] = useState(10);
+
+  const authHeader = useAuthHeader();
+  const { showToast, toastMessage, hideToastMessage } = useToastContext();
+
+  useEffect(() => {
+    if (showToast) {
+      toast.success(toastMessage, {
+        onClose: hideToastMessage,
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 3000,
+      });
+    }
+  }, [showToast, toastMessage, hideToastMessage]);
+
+  useEffect(() => {
+    fetchProgram(0, pageSize);
+  }, [pageSize]);
+
+  async function fetchProgram(offset, limit) {
+    const url = `${baseUrl}/program/list?offset=${offset}&limit=${limit}`;
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: authHeader(),
+    };
+
+    try {
+      const data = await makeRequest(url, headers);
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+  }
+
+  const columnHelper = createColumnHelper();
   const columns = [
     columnHelper.accessor("id", {
+      header: () => <span>ID</span>,
       cell: (info) => info.getValue(),
-      footer: (info) => info.column.id,
     }),
-    columnHelper.accessor((row) => row.program, {
+    columnHelper.accessor((row) => row.title, {
       id: "program",
       cell: (info) => <i>{info.getValue()}</i>,
       header: () => <span>Program</span>,
@@ -55,14 +96,12 @@ function ProgramTable() {
               icon={<EyeIcon className="w-4 h-4" />}>
               Lihat
             </Button>
-            <AlertDialog.Trigger asChild>
-              <Button
-                className="text-sm font-normal"
-                textColor="text-red-500"
-                icon={<TrashIcon className="w-4 h-4" />}>
-                Hapus
-              </Button>
-            </AlertDialog.Trigger>
+            <Button
+              className="text-sm font-normal"
+              textColor="text-red-500"
+              icon={<TrashIcon className="w-4 h-4" />}>
+              Hapus
+            </Button>
           </div>
         );
       },
@@ -84,6 +123,7 @@ function ProgramTable() {
   });
 
   function onPageSizeChanged({ value, label }) {
+    setPageSize(Number(value));
     table.setPageSize(Number(value));
   }
 
@@ -95,92 +135,91 @@ function ProgramTable() {
 
   return (
     <>
-      <AlertDialog.Root>
-        <div className="flex justify-between">
-          <h1 className="text-2xl font-semibold">Program</h1>
-          <Link to="create">
-            <Button
-              background="bg-primary"
-              textColor={"text-white"}
-              icon={<PlusIcon className="w-4 h-4" />}>
-              Tambah Program
-            </Button>
-          </Link>
-        </div>
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-semibold">Program</h1>
+        <Link to="create">
+          <Button
+            background="bg-primary"
+            textColor={"text-white"}
+            icon={<PlusIcon className="w-4 h-4" />}>
+            Tambah Program
+          </Button>
+        </Link>
+      </div>
 
-        <div className="flex justify-between mt-6">
-          <div className="flex space-x-3">
-            {/* Sorting Dropdown */}
-            <div>
-              <Dropdown
-                onSelect={onSorting}
-                defaultValue="A - Z"
-                label="Urutkan:">
-                <Dropdown.Items>
-                  <li
-                    value="asc"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    A - Z
-                  </li>
-                  <li
-                    value="desc"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    Z - A
-                  </li>
-                </Dropdown.Items>
-              </Dropdown>
-            </div>
-
-            {/* Page Size Dropdown */}
-            <div>
-              <Dropdown
-                onSelect={onPageSizeChanged}
-                defaultValue="10"
-                label="Tampilkan:"
-                endLabel="Entri">
-                <Dropdown.Items>
-                  <li
-                    value="10"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    10
-                  </li>
-                  <li
-                    value="50"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    50
-                  </li>
-                  <li
-                    value="100"
-                    className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
-                    100
-                  </li>
-                </Dropdown.Items>
-              </Dropdown>
-            </div>
+      <div className="flex justify-between mt-6">
+        <div className="flex space-x-3">
+          {/* Sorting Dropdown */}
+          <div>
+            <Dropdown
+              onSelect={onSorting}
+              defaultValue="A - Z"
+              label="Urutkan:">
+              <Dropdown.Items>
+                <li
+                  value="asc"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  A - Z
+                </li>
+                <li
+                  value="desc"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  Z - A
+                </li>
+              </Dropdown.Items>
+            </Dropdown>
           </div>
 
-          <div className="relative w-1/3">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <MagnifyingGlassIcon className="w-4 h-4" />
-            </div>
-            <input
-              type="search"
-              id="search"
-              className="bg-gray-50 text-light-gray border-none text-sm rounded-lg focus:ring-0 block w-full pl-10 p-2.5 shadow"
-              placeholder="Pencarian"
-            />
+          {/* Page Size Dropdown */}
+          <div>
+            <Dropdown
+              onSelect={onPageSizeChanged}
+              defaultValue="10"
+              label="Tampilkan:"
+              endLabel="Entri">
+              <Dropdown.Items>
+                <li
+                  value="10"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  10
+                </li>
+                <li
+                  value="50"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  50
+                </li>
+                <li
+                  value="100"
+                  className="block px-4 py-2 font-semibold cursor-pointer hover:bg-gray-100">
+                  100
+                </li>
+              </Dropdown.Items>
+            </Dropdown>
           </div>
         </div>
 
-        <div className="w-full h-full mt-6 bg-white rounded-lg">
-          <Table
-            className="mt-6"
-            table={table}
-            columns={columns}
-            data={data}
+        <div className="relative w-1/3">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <MagnifyingGlassIcon className="w-4 h-4" />
+          </div>
+          <input
+            type="search"
+            id="search"
+            className="bg-gray-50 text-light-gray border-none text-sm rounded-lg focus:ring-0 block w-full pl-10 p-2.5 shadow"
+            placeholder="Pencarian"
           />
         </div>
-      </AlertDialog.Root>
+      </div>
+
+      <div className="w-full h-full mt-6 bg-white rounded-lg">
+        <Table
+          className="mt-6"
+          table={table}
+          columns={columns}
+          data={data}
+        />
+      </div>
+      <ToastContainer />
     </>
   );
 }
