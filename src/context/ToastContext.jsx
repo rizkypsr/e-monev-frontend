b3818f4ef1606b418eeac05a4fbdc97eb5ceacae
rizkypsr <rizkypsrr@gmail.com>
2023-03-14 07:19:47 +0700

    implement new toast

--- src/context/ToastContext.jsx
+++ src/context/ToastContext.jsx
@@ -1,31 +1,67 @@
-import { createContext, useContext, useState } from "react";
+// import { createContext, useContext, useState } from "react";
+// import showToast from "../utils/showToast";
 
-export const ToastContext = createContext();
+import { createContext, useContext, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import showToastMessage from "../utils/showToast";
+
+// export const ToastContext = createContext();
+
+// export const useToastContext = () => useContext(ToastContext);
+
+// export const ToastProvider = ({ children }) => {
+//   const [toastMessage, setToastMessage] = useState("");
+//   const [toastType, setToastType] = useState("");
+
+//   const showToastMessage = (message, type = "success") => {
+//     setToastType(type);
+//     setToastMessage(message);
+//   };
+
+//   const hideToastMessage = () => {
+//     setToastType("");
+//     setToastMessage("");
+//   };
+
+//   return (
+//     <ToastContext.Provider
+//       value={{ showToastMessage, showToast, hideToastMessage }}>
+//       {children}
+//       {toastType && showToast(toastType, toastMessage, hideToastMessage)}
+//     </ToastContext.Provider>
+//   );
+// };
+
+export const ToastContext = createContext(null);
 
 export const useToastContext = () => useContext(ToastContext);
 
-export const ToastProvider = ({ children }) => {
+const ToastProvider = ({ children }) => {
   const [showToast, setShowToast] = useState(false);
+  const [toastType, setToastType] = useState("");
   const [toastMessage, setToastMessage] = useState("");
 
-  const showToastMessage = (message) => {
-    setShowToast(true);
+  const showToastMsg = (message, type = "success") => {
+    setToastType(type);
     setToastMessage(message);
+    setShowToast(true);
   };
 
   const hideToastMessage = () => {
     setShowToast(false);
-    setToastMessage("");
-  };
-
-  const value = {
-    showToast,
-    toastMessage,
-    showToastMessage,
-    hideToastMessage,
   };
 
   return (
-    <ToastContext.Provider value={value}>{children}</ToastContext.Provider>
+    <ToastContext.Provider
+      value={{ showToast, showToastMessage: showToastMsg, hideToastMessage }}>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={4000}
+      />
+      {children}
+      {showToast && showToastMessage(toastType, toastMessage, hideToastMessage)}
+    </ToastContext.Provider>
   );
 };
+
+export default ToastProvider;
---
+++ src/utils/showToast.js
@@ -0,0 +1,23 @@
+import { toast } from "react-toastify";
+
+export default function showToastMessage(type, message, onClose) {
+  const config = {
+    onClose,
+    position: toast.POSITION.BOTTOM_CENTER,
+    autoClose: 3000,
+  };
+
+  switch (type) {
+    case "info":
+      toast.info(message, config);
+      break;
+    case "warning":
+      toast.warning(message, config);
+      break;
+    case "error":
+      toast.error(message, config);
+      break;
+    default:
+      toast.success(message, config);
+  }
+}
