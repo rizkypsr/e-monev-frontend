c7fbe8b327e01d929fe96a98a803016900632305
rizkypsr <rizkypsrr@gmail.com>
2023-03-14 07:20:06 +0700

    change baseUrl to local

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const baseUrl = "https://e-monev-production.up.railway.app";
+// export const baseUrl = "https://e-monev-production.up.railway.app";
+export const baseUrl = "http://localhost:8080";
