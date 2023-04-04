a00086ecd029486a7ad9411ce45149be85214912
rizkypsr <rizkypsrr@gmail.com>
2023-04-04 17:35:04 +0300

    refactor Dashboard component

--- src/views/Admin/Dashboard/Dashboard.jsx
+++ src/views/Admin/Dashboard/Dashboard.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useAuthHeader } from 'react-auth-kit';
-import CountUp from 'react-countup';
 import { getCounts } from '../../../api/admin/dashboard';
 import ErrorPage from '../../ErrorPage';
+import CountBox from './components/CountBox';
 
 function Dashboard() {
   const [counts, setCounts] = useState({
@@ -15,15 +15,15 @@ function Dashboard() {
   });
   const [error, setError] = useState(null);
 
-  const authHeader = useAuthHeader();
+  const authHeaderRef = useRef(useAuthHeader());
 
   useEffect(() => {
     async function fetchCounts() {
       try {
-        const countsData = await getCounts(authHeader);
+        const countsData = await getCounts(authHeaderRef.current);
         setCounts(countsData);
-      } catch (error) {
-        setError(error.message);
+      } catch (err) {
+        setError(err.message);
       }
     }
 
@@ -42,78 +42,36 @@ function Dashboard() {
       </h1>
 
       <div className="bg-white rounded-lg grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-7 p-8 shadow-2xl shadow-[#F3F6FF]">
-        <div className="sm:w-60 h-20 bg-white shadow-md p-4 flex space-x-4 rounded-lg hover:solid-shadow-blue">
-          <div
-            className="w-12 h-12 rounded-lg"
-            style={{ background: '#56CCF2' }}
-          ></div>
-          <div>
-            <h2 className="font-semibold text-lg">
-              <CountUp end={counts.occasionCount} duration={0.7} />
-            </h2>
-            <h3 className="text-sm">URUSAN</h3>
-          </div>
-        </div>
-        <div className="sm:w-60 h-20 bg-white shadow-md p-4 flex space-x-4 rounded-lg hover:solid-shadow-purple">
-          <div
-            className="w-12 h-12 rounded-lg"
-            style={{ background: '#BB6BD9' }}
-          ></div>
-          <div>
-            <h2 className="font-semibold text-lg">
-              <CountUp end={counts.organizationCount} duration={0.7} />
-            </h2>
-            <h3 className="text-sm">ORGANISASI</h3>
-          </div>
-        </div>
-        <div className="sm:w-60 h-20 bg-white shadow-md p-4 flex space-x-4 rounded-lg hover:solid-shadow-green">
-          <div
-            className="w-12 h-12 rounded-lg"
-            style={{ background: '#6FCF97' }}
-          ></div>
-          <div>
-            <h2 className="font-semibold text-lg">
-              <CountUp end={counts.programCount} duration={0.7} />
-            </h2>
-            <h3 className="text-sm">PROGRAM</h3>
-          </div>
-        </div>
-        <div className="sm:w-60 h-20 bg-white shadow-md p-4 flex space-x-4 rounded-lg hover:solid-shadow-yellow">
-          <div
-            className="w-12 h-12 rounded-lg"
-            style={{ background: '#F2C94C' }}
-          ></div>
-          <div>
-            <h2 className="font-semibold text-lg">
-              <CountUp end={counts.activityCount} duration={0.7} />
-            </h2>
-            <h3 className="text-sm">KEGIATAN</h3>
-          </div>
-        </div>
-        <div className="sm:w-60 h-20 bg-white shadow-md p-4 flex space-x-4 rounded-lg hover:solid-shadow-orange">
-          <div
-            className="w-12 h-12 rounded-lg"
-            style={{ background: '#F2C94C' }}
-          ></div>
-          <div>
-            <h2 className="font-semibold text-lg">
-              <CountUp end={counts.purposeCount} duration={0.7} />
-            </h2>
-            <h3 className="text-sm">SASARAN</h3>
-          </div>
-        </div>
-        <div className="sm:w-60 h-20 bg-white shadow-md p-4 flex space-x-4 rounded-lg hover:solid-shadow-gray">
-          <div
-            className="w-12 h-12 rounded-lg"
-            style={{ background: '#BDBDBD' }}
-          ></div>
-          <div>
-            <h2 className="font-semibold text-lg">
-              <CountUp end={counts.userCount} duration={0.7} />
-            </h2>
-            <h3 className="text-sm">LOGIN AKSES USER</h3>
-          </div>
-        </div>
+        <CountBox
+          count={counts.occasionCount}
+          color="#56CCF2"
+          hoverColor="solid-shadow-blue"
+        />
+        <CountBox
+          count={counts.organizationCount}
+          color="#BB6BD9"
+          hoverColor="solid-shadow-purple"
+        />
+        <CountBox
+          count={counts.programCount}
+          color="#6FCF97"
+          hoverColor="solid-shadow-green"
+        />
+        <CountBox
+          count={counts.activityCount}
+          color="#F2C94C"
+          hoverColor="solid-shadow-yellow"
+        />
+        <CountBox
+          count={counts.purposeCount}
+          color="#F2994A"
+          hoverColor="solid-shadow-orange"
+        />
+        <CountBox
+          count={counts.userCount}
+          color="#BDBDBD"
+          hoverColor="solid-shadow-gray"
+        />
       </div>
     </>
   );
