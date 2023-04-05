2aee82e4c2675d3d7e28bb3ec87d836523b2ef1f
rizkypsr <rizkypsrr@gmail.com>
2023-04-05 06:20:41 +0300

    Refactor from hoverColor to className

--- src/views/Admin/Dashboard/components/CountBox.jsx
+++ src/views/Admin/Dashboard/components/CountBox.jsx
@@ -1,25 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import CountUp from 'react-countup';
+import { Link } from 'react-router-dom';
 
-export default function CountBox({ count, color, hoverColor }) {
+export default function CountBox({ className, linkTo, title, count, color }) {
   return (
-    <div
-      className={`sm:w-60 h-20 bg-white shadow-md p-4 flex space-x-4 rounded-lg hover:${hoverColor}`}
+    <Link
+      to={linkTo}
+      className={`sm:w-60 h-20 bg-white shadow-md p-4 flex space-x-4 rounded-lg ${className}`}
     >
       <div className="w-12 h-12 rounded-lg" style={{ background: color }} />
       <div>
         <h2 className="font-semibold text-lg">
           <CountUp end={count} duration={0.7} />
         </h2>
-        <h3 className="text-sm">URUSAN</h3>
+        <h3 className="text-sm uppercase">{title}</h3>
       </div>
-    </div>
+    </Link>
   );
 }
 
 CountBox.propTypes = {
+  className: PropTypes.string.isRequired,
+  linkTo: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   count: PropTypes.number.isRequired,
   color: PropTypes.string.isRequired,
-  hoverColor: PropTypes.string.isRequired,
 };
