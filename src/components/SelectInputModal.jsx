ac1fea8cde56644dddb64485ef5e64bc5e803472
rizkypsr <rizkypsrr@gmail.com>
2023-04-05 05:48:55 +0300

    add error message

--- src/components/SelectInputModal.jsx
+++ src/components/SelectInputModal.jsx
@@ -1,15 +1,34 @@
+import PropTypes from 'prop-types';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
-function SelectInputModal({ className, selectedValue, label }) {
+function SelectInputModal({ className, selectedValue, label, error }) {
   return (
-    <div
-      className={`flex text-sm items-center justify-between border border-dark-gray rounded-lg p-2.5 leading-4 text-dark-gray capitalize w-full ${className}`}
-    >
-      <span>{selectedValue || label}</span>
-      <ChevronDownIcon className="w-3 h-3" />
-    </div>
+    <>
+      <div
+        className={`flex text-sm items-center justify-between border border-dark-gray rounded-lg p-2.5 leading-4 text-dark-gray capitalize w-full ${className} ${
+          error ? 'border-red-600' : ''
+        }`}
+      >
+        <span>{selectedValue || label}</span>
+        <ChevronDownIcon className="w-3 h-3" />
+      </div>
+      {error && <p className="mt-2 text-xs text-red-600 text-left">{error}</p>}
+    </>
   );
 }
+
+SelectInputModal.propTypes = {
+  className: PropTypes.string,
+  error: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  selectedValue: PropTypes.string,
+};
+
+SelectInputModal.defaultProps = {
+  className: null,
+  error: null,
+  selectedValue: null,
+};
 
 export default SelectInputModal;
