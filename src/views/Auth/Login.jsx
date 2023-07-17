a64284283709e827397c0a8fb4352e127f731b5a
rizkypsr <rizkypsr@gmail.com>
2023-07-18 06:32:58 +0700

    add image carousel

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import { useAuthUser, useIsAuthenticated, useSignIn } from 'react-auth-kit';
 import { Navigate } from 'react-router-dom';
+import Slider from 'react-slick';
 import Logo from '../../assets/images/big_logo.png';
 import Label from '../../components/Label';
 import TextInput from '../../components/TextInput';
 import Button from '../../components/Button';
-import Batik from '../../assets/images/batik.png';
+import Img1 from '../../assets/images/img1.jpeg';
+import Img2 from '../../assets/images/img2.jpeg';
+import Img3 from '../../assets/images/img3.jpeg';
+import Img4 from '../../assets/images/img4.jpeg';
 import login from '../../api/auth/login';
-import showToastMsg from '../../utils/showToast';
 import { useToastContext } from '../../context/ToastContext';
 import Loading from '../../components/Loading';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -44,7 +49,7 @@ function Login() {
       ) {
         setLoading(false);
       } else {
-        showToastMsg('error', 'Terjadi kesalahan saat login');
+        showToastMessage('Terjadi kesalahan saat login', 'error');
       }
     } catch (error) {
       switch (error.message) {
@@ -69,86 +74,119 @@ function Login() {
 
     return <Navigate to="/" replace />;
   }
+
   return (
     <div className="lg:flex h-screen">
-      <div className="hidden lg:block h-full sm:basis-1/2">
-        <img src={Batik} className="object-cover w-full h-full" alt="Batik" />
+      <style>{`
+        .slick-dots {
+          position: absolute;
+          bottom: 20px;
+          left: 50%;
+          transform: translateX(-50%);
+          z-index: 1;
+        }
+      `}</style>
+      <div className="hidden lg:block h-full sm:w-1/2">
+        <Slider
+          dots
+          infinite
+          slidesToShow={1}
+          slidesToScroll={1}
+          speed={500}
+          arrows={false}
+          adaptiveHeight
+          autoplay
+          autoplaySpeed={5000}
+        >
+          {[Img1, Img2, Img3, Img4].map((img, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index}>
+              <img
+                src={img}
+                alt={`Gambar ${index + 1}`}
+                className="h-screen w-full object-cover"
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
-      <div className="flex flex-col basis-1/2 p-9">
-        <div className="flex space-x-3">
-          <img src={Logo} className="w-14" alt="Logo" />
-          <div className="text-dark-gray">
-            <h1 className="font-semibold">SISTEM INFORMASI E-MONEY</h1>
-            <h2>KABUPATEN SORONG</h2>
+      <div className="w-full lg:w-1/2 flex justify-center p-9">
+        <div className="w-full flex flex-col">
+          <div className="flex space-x-3">
+            <img src={Logo} className="w-14" alt="Logo" />
+            <div className="text-dark-gray">
+              <h1 className="font-semibold">SISTEM INFORMASI E-MONEY</h1>
+              <h2>KABUPATEN SORONG</h2>
+            </div>
           </div>
-        </div>
 
-        <div className="self-center mt-32 w-full sm:w-2/3 xl:w-1/2">
-          <h3 className="text-2xl font-semibold text-dark-gray">
-            Selamat Datang
-          </h3>
-          <h4 className="text-light-gray">
-            Login dibawah untuk akses akun Anda
-          </h4>
+          <div className="self-center mt-32 w-full sm:w-2/3 xl:w-1/2">
+            <h3 className="text-2xl font-semibold text-dark-gray">
+              Selamat Datang
+            </h3>
+            <h4 className="text-light-gray">
+              Login dibawah untuk akses akun Anda
+            </h4>
 
-          <form className="mt-4" onSubmit={onSubmit}>
-            <div className="mb-6">
-              <Label htmlFor="username">Username</Label>
-              <TextInput
-                id="username"
-                className="mt-2"
-                name="username"
-                placeholder="Masukan Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                error={usernameError}
-                required
-              />
-            </div>
-            <div className="mb-6">
-              <Label htmlFor="password">Password</Label>
-              <TextInput
-                id="password"
-                className="mt-2"
-                type="password"
-                name="password"
-                value={password}
-                placeholder="Masukan Password"
-                onChange={(e) => setPassword(e.target.value)}
-                error={passwordError}
-                required
-              />
-            </div>
-            <div className="flex items-center justify-between mb-6">
-              <div className="flex items-center">
-                <input
-                  id="remember"
-                  type="checkbox"
-                  className="w-4 h-4 border rounded border-light-gray focus:ring-3 focus:ring-blue-300"
+            <form className="mt-4" onSubmit={onSubmit}>
+              <div className="mb-6">
+                <Label htmlFor="username">Username</Label>
+                <TextInput
+                  id="username"
+                  className="mt-2"
+                  name="username"
+                  placeholder="Masukan Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  error={usernameError}
+                  required
                 />
-                <Label htmlFor="remember" className="ml-2">
-                  Ingatkan Saya
-                </Label>
+              </div>
+              <div className="mb-6">
+                <Label htmlFor="password">Password</Label>
+                <TextInput
+                  id="password"
+                  className="mt-2"
+                  type="password"
+                  name="password"
+                  value={password}
+                  placeholder="Masukan Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  error={passwordError}
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between mb-6">
+                <div className="flex items-center">
+                  <input
+                    id="remember"
+                    type="checkbox"
+                    className="w-4 h-4 border rounded border-light-gray focus:ring-3 focus:ring-blue-300"
+                  />
+                  <Label htmlFor="remember" className="ml-2">
+                    Ingatkan Saya
+                  </Label>
+                </div>
+
+                <button type="button" className="text-sm text-primary">
+                  Lupa Password
+                </button>
               </div>
 
-              <button type="button" className="text-sm text-primary">
-                Lupa Password
-              </button>
-            </div>
-
-            {loading ? (
-              <Loading />
-            ) : (
-              <Button
-                className="w-28"
-                type="submit"
-                background="bg-primary"
-                textColor="text-white"
-              >
-                Masuk
-              </Button>
-            )}
-          </form>
+              {loading ? (
+                <Loading />
+              ) : (
+                <Button
+                  className="w-28"
+                  type="submit"
+                  background="bg-primary"
+                  textColor="text-white"
+                >
+                  Masuk
+                </Button>
+              )}
+            </form>
+          </div>
         </div>
       </div>
     </div>
