5385712d0faab1ba2ec8672b8b89b3893caebb98
Muhammad Rifqi <rifqi.persie@gmail.com>
2024-02-18 22:59:54 +0700

    add docker

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { AuthProvider } from 'react-auth-kit';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
 import ToastProvider from './context/ToastContext';
-import router from './router';
 import store from './redux/store';
+import router from './router';
 
 const queryClient = new QueryClient({
   defaultOptions: {
