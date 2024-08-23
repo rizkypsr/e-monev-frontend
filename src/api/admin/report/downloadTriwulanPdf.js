fe7ba24db6e36833dd091ebef4a27969a56fe083
rizkypsr <rizkypsrr@gmail.com>
2024-08-23 18:35:05 +0700

    feat: add pdf export

---
+++ src/api/admin/report/downloadTriwulanPdf.js
@@ -0,0 +1,28 @@
+import axiosClient from '../../../config/axios';
+
+async function downloadTriwulanPdf(params, token) {
+  try {
+    const response = await axiosClient.get(
+      '/data-report/user/data-triwulan/pdf',
+      {
+        params,
+        headers: {
+          Authorization: token,
+        },
+        responseType: 'blob',
+      }
+    );
+
+    const responseData = response.data;
+
+    if (response.status !== 200) {
+      throw new Error(responseData.message);
+    }
+
+    return responseData;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+export default downloadTriwulanPdf;
