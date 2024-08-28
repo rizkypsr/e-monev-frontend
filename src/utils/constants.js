d7de8ae44bada0f2653c84fe8723814247717d20
rizkypsr <rizkypsrr@gmail.com>
2024-08-28 19:23:19 +0700

    feat: add button pdf and excel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,4 @@ export const baseUrl = 'https://e-montirpemda-api.kabupatensorong.id';
 export const domainUrl = 'http://localhost:5173';
 
 export const baseUrlAPI = 'https://e-montirpemda-api.kabupatensorong.id/';
+// export const baseUrlAPI = 'https://3458-110-136-219-228.ngrok-free.app/';
