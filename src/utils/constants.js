88b7acf5ad5f62c61a8451d4573678bc149b374e
rizkypsr <rizkypsrr@gmail.com>
2024-10-18 01:14:26 +0700

    chore: move static

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,28 @@
 // eslint-disable-next-line import/prefer-default-export
 export const baseUrlAPI = import.meta.env.VITE_BASE_API_URL;
+
+export const sorting = [
+  {
+    id: 'terbaru',
+    name: 'Terbaru',
+  },
+  {
+    id: 'terlama',
+    name: 'Terlama',
+  },
+];
+
+export const pageSizes = [
+  {
+    id: 10,
+    name: '10',
+  },
+  {
+    id: 50,
+    name: '50',
+  },
+  {
+    id: 100,
+    name: '100',
+  },
+];
